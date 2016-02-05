aaed1a5678882e3c07ea7d3dbc6e833295307d86
priyank5590 <vyaspriyank5@gmail.com>
2016-02-04 16:39:16 -0800

    header part updated

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 // JavaScript Document
+
+"use strict";
+
+// setuo your IIFE (Immediately Invoked Function Expression)
+
+(function () {
+
+	console.log("App Started...");
+	
+	//declared a named function way
+	function replaceFirstParagraph() {
+		console.log("inside replaceFirstParagraph function");
+		var firstParagraph;
+
+		firstParagraph = document.getElementById("firstParagraph");
+		firstParagraph.innerHTML = "My New Paragraph Data";
+	}
+	
+	})();
