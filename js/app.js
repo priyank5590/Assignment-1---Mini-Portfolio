a11fd0e80fa25dd46c629ebc79d0fd82795cf0e1
priyank5590 <vyaspriyank5@gmail.com>
2016-02-07 13:54:32 -0800

    aboutme and contact section added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-// JavaScript Document
-
 "use strict";
 
 // setuo your IIFE (Immediately Invoked Function Expression)
@@ -9,12 +7,25 @@
 	console.log("App Started...");
 	
 	//declared a named function way
-	function replaceFirstParagraph() {
-		console.log("inside replaceFirstParagraph function");
-		var firstParagraph;
-
-		firstParagraph = document.getElementById("firstParagraph");
-		firstParagraph.innerHTML = "My New Paragraph Data";
+	function ProjectDescription() {
+		console.log("inside ProjectDescription function");
+		
+		var firstProject, secondProject, thirdProject, aboutme;
+		
+		firstProject = document.getElementById("firstProject");
+		firstProject.innerHTML = "This was my final project in the first semester for web and internet fundamentels. Perticularly in this project I learned how to use jQuery slider and integrate google maps on website. ";
+		
+		secondProject = document.getElementById("secondProject");
+		secondProject.innerHTML = "It is an ecommerce application that allows users to shop bags through the mobile app.The application interface is simple and fluid, giving the users a smooth and satisfactory feel while using the application. ";
+		
+		thirdProject = document.getElementById("thirdProject");
+		thirdProject.innerHTML = "The team collaboration and communication app displays a user interface that I have perticularly designed to keep the workflow clear, straightforward, and as elegant as possible.";
+		
+		aboutme = document.getElementById("aboutme");
+		aboutme.innerHTML = "Hi, I'm priyank vyas. A web/User Interface designer from India. I have completed my Bachelor of Engineering in India where I worked as a web/UI designer in one of the best mobile and web application development company of Gujarat for more than 2 years. where I designed various application for android ,iOS and web. But because of my strong desire to learn Web development I enrolled for the course of interactive web design and development in Georgian college, Barrie, Ontario";
 	}
-	
-	})();
+		ProjectDescription();
+
+
+
+})();
